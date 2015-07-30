73d1af3ba2659562dc17e1d0cd1250bd77be0101
Yu Hoshino <yuhoshino@dev.local>
2015-07-31 05:20:26 +0900

    modified for localhost

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'taskapp'
 set :repo_url, 'https://github.com/yhoshino11/taskapp.git'
-set :branch, 'master'
+set :branch, 'develop'
 set :deploy_to, '/home/vagrant/taskapp'
 
 set :keep_releases, 1
@@ -10,6 +10,7 @@ set :rbenv_ruby, '2.1.6'
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets)
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, 'config/unicorn.rb'
 set :unicorn_rack_env, 'production'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,14 @@
 set :stage, :production
 set :rails_env, 'production'
 key_path = '~/.ssh/taskapp'
-server '192.168.179.11', roles: %w(web app db),
-                         user: 'vagrant',
-                         ssh_options: {
-                           user: 'vagrant',
-                           keys: key_path,
-                           auth_methods: %w(publickey)
-                         }
+server '127.0.0.1', port: 2201,
+                    roles: %w(web app db),
+                    user: 'vagrant',
+                    ssh_options: {
+                      user: 'vagrant',
+                      keys: key_path,
+                      auth_methods: %w(publickey)
+                    }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
