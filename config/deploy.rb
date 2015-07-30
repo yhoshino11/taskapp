69bbe1e678af5d7435571bfeb8fb57d724adc110
Yu Hoshino <yuhoshino@dev.local>
2015-07-31 06:05:15 +0900

    link config/secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rails_env, 'production'
 set :rbenv_ruby, '2.1.6'
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
-set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets)
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets config/secrets.yml)
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, 'config/unicorn.rb'
